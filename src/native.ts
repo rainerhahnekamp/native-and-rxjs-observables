e6c599f1f6e43932a2604d2dc07f78a46925af7c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-04-24 22:49:27 +0200

    fourth part

--- src/native.ts
+++ src/native.ts
@@ -1,20 +1,23 @@
 const input = document.getElementById("input")! as HTMLInputElement;
 const button = document.getElementById("button")! as HTMLButtonElement;
 
+const abortController = new AbortController();
+
 input
   .when<InputEvent>("input")
   .map((event) => (event.target as HTMLInputElement).value)
   .filter((value) => value.length > 2)
-  // .inspect((value) => {
-  //   if (value === "RxJS") {
-  //     throw new Error("invald input");
-  //   }
-  // })
-  .catch(() => Observable.from(["-"]))
-  .take(2)
   .finally(() => console.log("We are done"))
-  .subscribe({
-    next: (event) => console.log(event),
-    complete: () => console.log("completed"),
-    error: (err) => console.log("error: %o", err),
-  });
+  .subscribe(
+    {
+      next: (event) => console.log(event),
+      complete: () => console.log("completed"),
+      error: (err) => console.log("error: %o", err),
+    },
+    { signal: abortController.signal },
+  );
+
+button.when("click").subscribe(() => {
+  console.log("unsubscribing...");
+  abortController.abort();
+});
--- src/rxjs.ts
+++ src/rxjs.ts
@@ -14,23 +14,19 @@ import {
 const input = document.getElementById("input")! as HTMLInputElement;
 const button = document.getElementById("button")! as HTMLButtonElement;
 
-fromEvent<InputEvent>(input, "input")
+const subscription = fromEvent<InputEvent>(input, "input")
   .pipe(
     map((event) => (event.target as HTMLInputElement).value),
     filter((value) => value.length > 2),
-    // take(2),
-    tap((value) => {
-      if (value === "RxJS") {
-        throw new Error("invalid input");
-      }
-    }),
-    take(2),
     finalize(() => console.log("We are done")),
-    // retry(),
-    // catchError(() => of("-")),
   )
   .subscribe({
     next: (event) => console.log(event),
     complete: () => console.log("completed"),
     error: (err) => console.log("error: %o", err),
   });
+
+fromEvent(button, "click").subscribe(() => {
+  console.log("unsubscribing...");
+  subscription.unsubscribe();
+});
