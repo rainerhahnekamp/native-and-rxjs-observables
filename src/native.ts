4d26723db87d2c066ec00c0fba07c225ba58303c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-04-24 22:55:28 +0200

    fifth part

--- src/native.ts
+++ src/native.ts
@@ -3,21 +3,8 @@ const button = document.getElementById("button")! as HTMLButtonElement;
 
 const abortController = new AbortController();
 
-input
+const promise = input
   .when<InputEvent>("input")
   .map((event) => (event.target as HTMLInputElement).value)
   .filter((value) => value.length > 2)
-  .finally(() => console.log("We are done"))
-  .subscribe(
-    {
-      next: (event) => console.log(event),
-      complete: () => console.log("completed"),
-      error: (err) => console.log("error: %o", err),
-    },
-    { signal: abortController.signal },
-  );
-
-button.when("click").subscribe(() => {
-  console.log("unsubscribing...");
-  abortController.abort();
-});
+  .last();
--- src/rxjs.ts
+++ src/rxjs.ts
@@ -3,7 +3,9 @@ import {
   debounceTime,
   filter,
   finalize,
+  firstValueFrom,
   fromEvent,
+  lastValueFrom,
   map,
   of,
   retry,
@@ -14,19 +16,12 @@ import {
 const input = document.getElementById("input")! as HTMLInputElement;
 const button = document.getElementById("button")! as HTMLButtonElement;
 
-const subscription = fromEvent<InputEvent>(input, "input")
-  .pipe(
+const promise = lastValueFrom(
+  fromEvent<InputEvent>(input, "input").pipe(
     map((event) => (event.target as HTMLInputElement).value),
     filter((value) => value.length > 2),
-    finalize(() => console.log("We are done")),
-  )
-  .subscribe({
-    next: (event) => console.log(event),
-    complete: () => console.log("completed"),
-    error: (err) => console.log("error: %o", err),
-  });
+    take(2),
+  ),
+);
 
-fromEvent(button, "click").subscribe(() => {
-  console.log("unsubscribing...");
-  subscription.unsubscribe();
-});
+promise.then((value) => console.log(value));
